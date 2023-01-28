dcc77ba99fa3dc3db91e6c329b725a783db80665
Jan Smejkal <jan.smejkal.osobne@gmail.com>
2023-01-28 09:06:48 +0100

    poc: fetch ssr and propagate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Hydrate, QueryClient, QueryClientProvider} from 'react-query';
+import {useMemo} from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    const queryClient = useMemo(() => new QueryClient(), [])
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import Head from 'next/head'
+import {GetServerSideProps} from 'next';
+import {QueryClient, dehydrate, DehydratedState, useQuery} from "react-query";
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+type HomeProps = {
+    dehydratedState: DehydratedState
+}
+
+export default function Home({dehydratedState}: HomeProps) {
+    const {data} = useQuery('episodes', () => fetch('https://rickandmortyapi.com/api/episode').then(async (data: Response)=>{
+        const parsed = await data.json()
+        const {info, results} = parsed
+        return {info, results}
+    }), {
+        refetchInterval: 1000
+    })
+    if (!data) {
+        return (<div>No Data</div>)
+    }
   return (
     <>
       <Head>
@@ -11,8 +27,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-
+          {data.info.count}
       </main>
     </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async (context ) => {
+
+    const queryClient = new QueryClient()
+
+    await queryClient.fetchQuery('episodes',() => fetch('https://rickandmortyapi.com/api/episode').then(async (data: Response)=>{
+        const parsed = await data.json()
+        const {info, results} = parsed
+        return {info, results}
+    }))
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        }
+    }
+};
