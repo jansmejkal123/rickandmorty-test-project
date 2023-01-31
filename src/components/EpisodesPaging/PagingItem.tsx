f573dc286237acddaa85084d7540a00e59d6e8a7
Jan Smejkal <jan.smejkal.osobne@gmail.com>
2023-01-31 03:54:00 +0100

    fix paging not working when js disabled

--- src/components/EpisodesPaging/PagingItem.tsx
+++ src/components/EpisodesPaging/PagingItem.tsx
@@ -1,6 +1,7 @@
 import Pagination from "react-bootstrap/Pagination";
 import {useRouter} from "next/router";
 import {MouseEventHandler} from "react";
+import Link from "next/link";
 
 type PagingItemProps = {
     pageNumber: number,
@@ -11,7 +12,7 @@ const PagingItem = ({pageNumber, isActive = false, isDisabled = false}: PagingIt
     const {replace} = useRouter()
     const onClickHandler: MouseEventHandler<HTMLButtonElement> = (e) => replace(`/episodes?page=${pageNumber}`)
     return (
-        <Pagination.Item key={pageNumber} active={isActive} disabled={isDisabled} onClick={onClickHandler}>{pageNumber}</Pagination.Item>
+        <Pagination.Item key={pageNumber} active={isActive} disabled={isDisabled} as={Link} href={`/episodes?page=${pageNumber}`} onClick={onClickHandler}>{pageNumber}</Pagination.Item>
     )
 }
 
